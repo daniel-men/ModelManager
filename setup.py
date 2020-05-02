ed44a548be787d47ce50372f23cb24397b253bf4
Daniel Mensing <danielmensing@Daniels-MacBook-Pro.local>
2020-05-02 23:10:26 +0200

    setup for pip package WIP

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(name="ModelManager", version='1.0', packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="KerasModelManager-pkg-dan_men",
+    version="0.0.1",
+    author="Daniel Mensing",
+    author_email="daniel.mensing@gmx.net",
+    description="A wrapper for Keras models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daniel-men/ModelManager",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
