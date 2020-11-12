991f9d2d21c69886e6ac0b5e274b294c14ba08bc
Daniel Mensing <daniel.mensing16@gmail.com>
2020-11-13 00:51:35 +0100

    Version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="KerasModelManager",
-    version="0.0.4",
+    version="0.0.5",
     author="Daniel Mensing",
     author_email="daniel.mensing@gmx.net",
     description="A wrapper for Keras models",
